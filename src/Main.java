32b6e14a72340ac0c247c34cd0e1a57b64151d3f
eduard <eduard42rus@mail.ru>
2023-03-13 20:47:37 +0700

    Lesson "Strings" is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+    private static void task1() {
+        System.out.println("Task 1");
+        String firstname = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstname + " " + middleName + " " + lastName;
+        System.out.println("Employee's name is " + fullName);
+        System.out.println("Task 2");
+        System.out.println(fullName.toUpperCase());
+    }
+    private static void task2() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Employee's name is " + fullName);
     }
 }
